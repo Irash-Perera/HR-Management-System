fc24311616a30b05ce88153664d6162bb6e3b43e
Laksara K.Y <129388843+YasiruLaksara@users.noreply.github.com>
2023-11-01 01:55:54 +0530

    Update ReportbyLeave.jsx
    Changed the implemetation

--- client/src/components/modules/ReportbyLeave.jsx
+++ client/src/components/modules/ReportbyLeave.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import '../styleAssets/Report.css';
 
 const DepartmentLeaves = () => {
   const [departmentId, setDepartmentId] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   const [Leaves, setLeaves] = useState([]);
 
   const fetchDepartmentLeaves = async () => {
     try {
-      const res = await axios.get("http://localhost:8800/leavebal/"+departmentId);
+      const res = await axios.get('http://localhost:8800/leavebal/', {
+        params: {  departmentId,startDate, endDate } // pass start and end dates  as parameters
+      });
       setLeaves(res.data);
     } catch (err) {
       console.log(err);
     }
   };
+  
 
   return (
     <div>
@@ -24,26 +31,45 @@ const DepartmentLeaves = () => {
         value={departmentId}
         onChange={(e) => setDepartmentId(e.target.value)}
       />
+
+      <div>
+        <label>Start Date: </label>
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+      </div>
+
+      <div>
+        <label>End Date: </label>
+        <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+      </div>
+
       <button onClick={fetchDepartmentLeaves}>Fetch Leaves</button>
 
       <table className='Employee-table'>
         <thead>
           <tr>
             <th>Department ID</th>
-            <th>Annual</th>
-            <th>Casual</th>
-            <th>Maternity</th>
-            <th>No-Pay</th>
+            <th>Employee ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Reason</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Duration</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
           {Leaves.map((Leave) => (
             <tr key={Leave.Dept_ID}>
               <td>{Leave.Dept_ID}</td>
-              <td>{Leave.Annual}</td>
-              <td>{Leave.Casual}</td>
-              <td>{Leave.Maternity}</td>
-              <td>{Leave.No_pay}</td>
+              <td>{Leave.Employee_ID}</td>
+              <td>{Leave.First_Name}</td>
+              <td>{Leave.Last_Name}</td>
+              <td>{Leave.Reason}</td>
+              <td>{Leave.Start_Date}</td>
+              <td>{Leave.End_Date}</td>
+              <td>{Leave.Duration}</td>
+              <td>{Leave.Status}</td>
             </tr>
           ))}
         </tbody>
