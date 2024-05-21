9562ba891cecc29d8c642f2ba2e8e95838421f30
Irash Perera <irash.21@cse.mrt.ac.lk>
2024-05-21 20:21:26 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,15 @@ import express from "express"
 import mysql from "mysql2"
 import cors from "cors"
 import bcrypt from "bcrypt";
+import dotenv from "dotenv"
 
+dotenv.config()
 const app = express()
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
